2245334f895ce6a6201a054fa4d869efd2c8bdfe
EMend17 <emendez0017@gmail.com>
2022-06-17 11:54:49 -0500

    Solucion de version javax.persitance

--- src/main/java/com/ipn/mx/modelo/entidades/Articulo.java
+++ src/main/java/com/ipn/mx/modelo/entidades/Articulo.java
@@ -5,12 +5,13 @@
  */
 package com.ipn.mx.modelo.entidades;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+
 import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
 import lombok.Data;
 import lombok.NoArgsConstructor;
